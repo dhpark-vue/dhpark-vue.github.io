a9e5427f65ad19552893ea62a91c85dc73397ca2
pdh4869 <pdh4869@gmail.com>
2024-01-31 11:39:44 +0900

    app.vue modify

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <script setup>
-import { ref } from 'vue'
-const count = ref(0)
+import { ref, onMounted } from 'vue'
+const count = ref({
+	cnt: 0,
+	name: "Park"
+})
+
+function increment() {
+	count.value.cnt++
+	count.value.name = count.value.name.split('').reverse().join('')
+}
+
+onMounted(() => console.log(`The initial count is ${count.value.cnt}.`))
 </script>
 
 <template>
-	<button @click="count++">Count is: {{ count }} </button>
+	<button @click="increment">Count is: {{ count.cnt > 10 ? "크다" : "작다" }} {{ count.name}} </button>
 </template>
 
 <style scoped>
