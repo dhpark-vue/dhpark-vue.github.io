c13ff9df0a4a3297bc3b7b4390c95553091edaec
pdh4869 <pdh4869@gmail.com>
2024-02-01 10:08:50 +0900

    computed / ref

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <script setup>
-import { ref } from 'vue'
-import shoes from '@/assets/shoes.json' 
+import { reactive, computed, ref } from 'vue'
 
-const shoe = ref(shoes)
+const author = ref({
+	name: 'John Doe',
+	books: [1, 2, 3]
+})
+// ref에는 author에 value 안붙고, computed에는 붙어야 함
+const publishedBooksMessage = computed(() => { return author.value.books.length > 2 ? 'Yes' : 'No'})
 </script>
 
 <template>
-	<h2>신발 재고</h2><br>
-	<hr/>
-	<li v-for="obj in shoe">
-		<span v-if="obj.brand=='ADIDAS'">
-			{{obj.id}} <br>
-			name: {{obj.name}} <br>
-			brand: {{obj.brand}} <br>
-			gender: {{obj.gender}} <br>
-			category: {{obj.category}} <br>
-			price: ${{obj.price}} <br>
-			is_in_inventory: {{obj.is_in_inventory}} <br>
-			items_left: {{obj.items_left}} <br> 
-			imageURL: {{obj.imageURL}} <br>
-			slug: {{obj.slug}} <br>
-			<hr/>
-		</span>
-	</li>
+	{{author.name}}
+	<p>Has published books: </p>	
+	<span>{{ author.books.length > 2 ? 'Yes' : 'No' }}</span>
+
+	<p>Has published books: {{publishedBooksMessage}}</p>
 </template>
