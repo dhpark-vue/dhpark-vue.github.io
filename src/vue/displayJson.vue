cb272ea78df41974159fd906146b63320d0d2816
pdh4869 <pdh4869@gmail.com>
2024-02-05 22:41:26 +0900

    vuetify test

---
+++ src/vue/displayJson.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, watch } from 'vue'
+
+const todoId = ref(1); const todoData = ref(null)
+
+async function fetchData() {
+    todoData.value = null
+    const res = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${todoId.value}`
+    )
+    todoData.value = await res.json()
+}
+fetchData()
+
+function get() {
+    todoId.value++
+}
+
+// 감사자
+watch(todoId, fetchData)
+</script>
+ 
+<template>
+    <p>Todo id: {{ todoId }}</p>
+    <button @click="get()" :disabled="!todoData">Fetch next todo</button>
+    <p v-if="!todoData">Loading....</p>
+    <pre v-else>{{ todoData }}</pre>
+</template>
